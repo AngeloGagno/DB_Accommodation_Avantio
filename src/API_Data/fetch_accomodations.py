c4e30bcfc068094231c2d7f6c3d810133f4033c4
AngeloGagno <angelogagno@gmail.com>
2025-02-26 12:52:08 -0300

    Alterando a url de requisição

--- src/API_Data/fetch_accomodations.py
+++ src/API_Data/fetch_accomodations.py
@@ -3,7 +3,7 @@ from API_Data.get_accomodation import get_accomodations
 def fetch_all_accommodations():
     """Percorre todas as páginas da API para coletar todas as acomodações"""
     id_list = []  
-    next_url = 'https://api.avantio.pro/pms/v2/accommodations'
+    next_url = 'https://api.avantio.pro/pms/v2/accommodations?status=ENABLED&status=DISABLED'
 
     while next_url:  
         data = get_accomodations(next_url)  
